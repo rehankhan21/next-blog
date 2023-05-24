91b4c219653bf529a7d0394412eb5c37baf226de
rehankhan21 <narutop224@gmail.com>
2023-05-24 03:52:13 -0400

    added more data

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -10,36 +10,33 @@ function PostContent(props) {
   const imagePath = `/images/posts/${post.slug}/${post.image} `;
 
   const customRenderers = {
-    image(image) {
-      return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          alt={image.alt}
-          width={600}
-          height={300}
-        />
-      );
-    },
-    // paragraph(paragrah) {
-    //   const { node } = paragrah;
-
-    //   if (node.children[0].type === "image") {
-    //     const image = node.children[0];
-
-    //     return (
-    //       <div className={classes.image}>
-    //         <Image
-    //           src={`/images/posts/${post.slug}/${image.url}`}
-    //           alt={image.alt}
-    //           width={600}
-    //           height={300}
-    //         />
-    //       </div>
-    //     );
-    //   }
-
-    //   return <p>{paragrah.children}</p>;
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
     // },
+    paragraph(paragrah) {
+      const { node } = paragrah;
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragrah.children}</p>;
+    },
   };
 
   return (
