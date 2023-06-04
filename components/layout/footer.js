f20196d5c07af7dfc3e8be523613af5ee036018b
rehankhan21 <narutop224@gmail.com>
2023-06-03 23:23:28 -0400

    added footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,4 +1,4 @@
-import styles from "./Footer.module.css";
+import styles from "./footer.module.css";
 import Image from "next/image";
 
 const Footer = () => (
@@ -7,6 +7,30 @@ const Footer = () => (
       <Image
         src="/images/site/github-mark-white.svg"
         alt="Github"
+        width={300}
+        height={200}
+      />
+    </a>
+    <a
+      href="https://www.credly.com/badges/8a86fff2-2f7c-4521-bd7a-0858e7f6089d/public_url"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <Image
+        src="/images/site/credly-svgrepo-com.svg"
+        alt="AWS Cert"
+        width={300}
+        height={200}
+      />
+    </a>
+    <a
+      href="https://www.linkedin.com/in/rehankhann/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <Image
+        src="/images/site/icons8-linkedin.svg"
+        alt="LinkedIn"
         width={300}
         height={200}
       />
