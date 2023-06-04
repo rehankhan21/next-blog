1109c3b6403b4f660425f46ad3886b2525c6077a
rehankhan21 <narutop224@gmail.com>
2023-06-04 13:54:45 -0400

    small style changes

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -15,6 +15,52 @@ function Hero() {
         />
       </div>
       <h1>Rehan Khan</h1>
+      <div>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/rehankhan21"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="/images/site/github-mark-white.svg"
+                alt="Github"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/rehankhann/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="/images/site/icons8-linkedin.svg"
+                alt="LinkedIn"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.credly.com/badges/8a86fff2-2f7c-4521-bd7a-0858e7f6089d/public_url"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="/images/site/credly-svgrepo-com.svg"
+                alt="AWS Cert"
+                width={50}
+                height={50}
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
       <p>
         I am a Full-Stack Developer with a Bachelors in Computer Science. I have
         a strong interest in developing quality software that meets client
--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -13,48 +13,6 @@ function MainNavigation(params) {
       <nav>
         <ul>
           <li>
-            <a
-              href="https://github.com/rehankhan21"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Image
-                src="/images/site/github-mark-white.svg"
-                alt="Github"
-                width={300}
-                height={200}
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/rehankhann/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Image
-                src="/images/site/icons8-linkedin.svg"
-                alt="LinkedIn"
-                width={300}
-                height={200}
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.credly.com/badges/8a86fff2-2f7c-4521-bd7a-0858e7f6089d/public_url"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Image
-                src="/images/site/credly-svgrepo-com.svg"
-                alt="AWS Cert"
-                width={300}
-                height={200}
-              />
-            </a>
-          </li>
-          <li>
             <Link href="/posts">Posts</Link>
           </li>
           <li>
