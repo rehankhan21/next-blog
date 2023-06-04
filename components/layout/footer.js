5ba0353687b6f6b61dc6af59660c21bd6ef92784
rehankhan21 <narutop224@gmail.com>
2023-06-03 23:21:26 -0400

    added socials to navbar

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Footer = () => (
   <footer className={styles.footer}>
-    {/* <a href="https://github.com/rehankhan21" target="_blank" rel="noreferrer">
+    <a href="https://github.com/rehankhan21" target="_blank" rel="noreferrer">
       <Image
         src="/images/site/github-mark-white.svg"
         alt="Github"
@@ -11,30 +11,6 @@ const Footer = () => (
         height={200}
       />
     </a>
-    <a
-      href="https://www.credly.com/badges/8a86fff2-2f7c-4521-bd7a-0858e7f6089d/public_url"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <Image
-        src="/images/site/credly-svgrepo-com.svg"
-        alt="AWS Cert"
-        width={300}
-        height={200}
-      />
-    </a>
-    <a
-      href="https://www.linkedin.com/in/rehankhann/"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <Image
-        src="/images/site/icons8-linkedin.svg"
-        alt="LinkedIn"
-        width={300}
-        height={200}
-      />
-    </a> */}
   </footer>
 );
 
